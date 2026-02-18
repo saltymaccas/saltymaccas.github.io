fe9951c8055b3482a027d1b53bc0e12b6af6fb01
suse <newtonm472@gmail.com>
2026-02-18 19:04:29 +1100

    refactor

---
+++ site.js
@@ -0,0 +1,16 @@
+message = `I just achieved Remote Code Execution in your Browser. ☺️ 
+              Get hacked lol :D
+              I'll even show you proof: 7*7 = ${7 * 7}`
+
+document.getElementById('rce-message-area').innerText = message
+
+async function select_saying() {
+  response = await fetch("sayyings");
+  text = await response.text()
+  sayings = text.split("%")
+  var selected_saying = await sayings[Math.floor(Math.random() * sayings.length)]
+  return selected_saying
+}
+
+select_saying().then(s => document.getElementById("quote-area").innerText = s)
+
